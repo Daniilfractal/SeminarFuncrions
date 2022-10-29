28d701300e1dd0cd2c44b8617917263e367b2981
Daniil <trofimovdan@gmail.com>
2022-10-29 09:21:44 +0300

    удалось решить вторую задачу

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -2,8 +2,27 @@
 
 Console.WriteLine("Введите число");
 int number = int.Parse(Console.ReadLine()!);
-
-if (number<100)
+bool lessthen100(int number)
 {
-Console.WriteLine("Третьей цифры нет");
+    return number < 100;
 }
+
+if (lessthen100(number))
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+else
+{
+    while (number > 999)
+    {
+        number = number/10;
+    }
+
+    int firstdigit = (number / 100);
+    int seconddigit = (number - firstdigit * 100) / 10;
+    int number2 = (number - firstdigit * 100);
+    int thirddigit = (number2 - seconddigit * 10);
+    Console.WriteLine(thirddigit);
+}
+
