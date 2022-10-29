62a2d46c209d5c7558c0a9d59ffb0c7011858360
Daniil <trofimovdan@gmail.com>
2022-10-29 06:43:48 +0300

    сделал 1 задачку

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Принимает на вход трёхзначное число и на выходе показать вторую цифру этого числа. 
+// Через строку решать нельзя.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+
+int number = int.Parse(Console.ReadLine()!);
+
+int firstdigit = (number/100);
+int seconddigit = (number - firstdigit*100) / 10;
+
+Console.WriteLine(seconddigit);
+
+
+
